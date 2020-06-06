12e9ae60602447d1d2a2f40f3e4eeaf41debf1b3
Serpod <serpod@pm.me>
2020-06-06 22:34:10 +0200

    Fix database interactions

--- tigris.py
+++ tigris.py
@@ -42,7 +42,7 @@ class TigrisBank():
         return balance
 
     def get_balance(self, user_id=None):
-        query_fetch = "SELECT * FROM {} WHERE user_id = ?".format(BALANCE_TABLE)
+        query_fetch = "SELECT balance FROM {} WHERE user_id = ?".format(BALANCE_TABLE)
         cur = self.db.cursor()
         cur.execute(query_fetch, (user_id,))
         balance = cur.fetchone()
@@ -50,7 +50,7 @@ class TigrisBank():
             log_error("(get_balance) user_id {} is not in the database".format(user_id))
             return -1 
 
-        return balance[1]
+        return balance[0]
 
     def new_account(self, user_id, balance=0.):
         if self.get_balance(user_id) >= 0:
@@ -121,7 +121,7 @@ class TigrisBank():
         query_max_job_id = "SELECT MAX(job_id) FROM {} WHERE user_id = ?".format(JOB_TABLE)
         cur = self.db.cursor()
         cur.execute(query_max_job_id, (user_id,))
-        max_job_id = cur.fetchone()
+        max_job_id = cur.fetchone()[0]
         if max_job_id is None:
             # First job
             job_id = 0
@@ -129,33 +129,37 @@ class TigrisBank():
             job_id = max_job_id + 1
 
         # Insert new job
-        query_new_job = "INSERT INTO {}(user_id, job_id, title, salary) VALUES(?, ?, ?, ?)"
+        query_new_job = "INSERT INTO {}(user_id, job_id, title, salary) VALUES(?, ?, ?, ?)".format(JOB_TABLE)
         cur = self.db.cursor()
         cur.execute(query_new_job, (user_id, job_id, title, salary))
         self.db.commit()
 
         return 0
 
-
-    def remove_job(self, user_id, job_id):
+    def get_job(self, user_id, job_id):
         query_fetch = "SELECT * FROM {} WHERE user_id = ? AND job_id = ?".format(JOB_TABLE)
         cur = self.db.cursor()
         cur.execute(query_fetch, (user_id, job_id))
         job = cur.fetchone()
+
+        return job
+
+    def remove_job(self, user_id, job_id):
+        job = self.get_job(user_id, job_id)
         if job is None:
-            log_error("(remove_job) The job ({}) for user_id {} doesn't exist".format(job_id, user_id))
-            return 1
+           log_error("(remove_job) The job ({}) for user_id {} doesn't exist".format(job_id, user_id))
+           return None
 
         query_delete = "DELETE FROM {} WHERE user_id = ? AND job_id = ?".format(JOB_TABLE)
         cur = self.db.cursor()
-        cur.execute(query_fetch, (user_id, job_id))
+        cur.execute(query_delete, (user_id, job_id))
         self.db.commit()
 
-        return 0
+        return job
 
 
     def get_jobs(self, user_id):
-        query_fetch = "SELECT * FROM {} WHERE user_id = ? ORDER BY job_id ASC".format(JOB_TABLE)
+        query_fetch = "SELECT user_id, job_id, title, salary FROM {} WHERE user_id = ? ORDER BY job_id ASC".format(JOB_TABLE)
         cur = self.db.cursor()
         cur.execute(query_fetch, (user_id, ))
         jobs = cur.fetchall()
@@ -164,7 +168,7 @@ class TigrisBank():
 
 
     def get_all_jobs(self):
-        query_fetch = "SELECT * FROM {} ORDER BY  job_id ASC, user_id".format(JOB_TABLE)
+        query_fetch = "SELECT * FROM {} ORDER BY  user_id ASC, job_id ASC".format(JOB_TABLE)
         cur = self.db.cursor()
         cur.execute(query_fetch)
         jobs = cur.fetchall()
@@ -176,7 +180,7 @@ class TigrisBank():
         query_fetch = "SELECT SUM(salary) FROM {} WHERE user_id = ?".format(JOB_TABLE)
         cur = self.db.cursor()
         cur.execute(query_fetch, (user_id,))
-        salary = cur.fetchone()
+        salary = cur.fetchone()[0]
         if salary is None:
             log_error("(get_salary) The user_id {} has no job".format(user_id))
             return None
